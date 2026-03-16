074a5e146d0cc8a1cb55753c660decce922d46e2
charlieost <charlie_ost@outlook.com>
2026-03-16 18:25:53 +0000

    Initialise factory and API specific contracts

---
+++ contracts/APIBazaarFactory.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+// FTGP Group 3 - API Bazaar
+// Contract creates and tracks individual API contracts, each associated with a provider
+// Calls upon APIProviderLogic to instantiate an individual contract, bonding curve, and token.
