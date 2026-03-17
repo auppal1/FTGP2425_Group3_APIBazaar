cb3d5714a0952fb379ba0ed9669794530df6bcfe
C Durden <ey23867@bristol.ac.uk>
2026-03-17 19:40:57 +0000

    Added functions for sale price, selling, receive and fallback

--- contracts/APIProviderLogic.sol
+++ contracts/APIProviderLogic.sol
@@ -55,34 +55,55 @@ contract TokenAPI is ERC20 {
     }
 
     // Function to calculate token purchase price using bonding curve
-    function getPurchasePrice(uint256 quantity) public returns(uint256) {
+    function getPurchasePrice(uint256 amount) public returns(uint256) {
+
+        require(amount > 0 && amount <= capacity, "Invalid Amount");
+        require(_totalSupply + amount < capacity, "Amount breaches capacity");
 
         // reset price to 0 so that the contract is only calculating price of new tokens
         purchasePrice = 0;
-        require(_totalSupply + quantity < capacity, "Amount breaches capacity");
 
         // Calculate price for requested number of tokens
         uint256 numerator = capacity + k - _totalSupply;
-        uint256 denominator = capacity + k - (_totalSupply + quantity);
-        purchasePrice = (b*_totalSupply) + a*(((numerator)/(denominator)).log2()) + quantity;
+        uint256 denominator = capacity + k - (_totalSupply + amount);
+        purchasePrice = b*_totalSupply) + a*(((numerator)/(denominator)).log2()) + amount;
 
         return purchasePrice;
     }
 
+    // Function to calculate current token price a user can expect to receive for
+    // selling tokens back to the contract
+    function getSalePrice(uint256 amount) public returns(uint256) {
+
+        require(amount > 0 && amount <= capacity, "Invalid Amount");
+        // require that user is not trying to sell more tokens than exist
+        require(amount <= _totalSupply, "Total token supply insufficient.");
+
+        // reset sale price to 0
+        salePrice = 0;
+
+        // Calculate price for requested number of tokens
+        uint256 numerator = capacity + k - _totalSupply;
+        uint256 denominator = capacity + k - (_totalSupply + amount);
+        salePrice = b*_totalSupply) + a*(((numerator)/(denominator)).log2()) - amount;
+
+        return salePrice;
+    }
+
     // Function to mint/enable buying of new tokens
-    function buyTokens(uint256 quantity) public payable {
+    function buyTokens(uint256 amount) public payable {
 
         address recipient = msg.sender; // address to send the bought tokens to
 
         // require that some tokens are being bought
-        require(quantity > 0, "Token quantity must be positive");
+        require(amount > 0, "Token quantity must be positive");
         // get the price of the requested quantity of tokens
-        purchasePrice = getPurchasePrice(quantity);
+        purchasePrice = getPurchasePrice(amount);
         // require that the user has sent enough money
         require(msg.value >= purchasePrice, "Insufficient WEI sent");
 
         // mint the requested/bought tokens and send them to the user
-        _mint(recipient, quantity);
+        _mint(recipient, amount);
         // update _totalSupply variable
         _totalSupply = totalSupply();
         // and update user's balance
@@ -94,5 +115,49 @@ contract TokenAPI is ERC20 {
         // purchasePrice getter function they do not see the value of the last
         // purchase
         purchasePrice = 0;
+    }
+
+    // Function to burn/enable selling of tokens
+    function sellTokens(uint256 amount) public {
+
+        address seller = msg.sender; // address selling tokens
+
+        // require that some tokens are being sold
+        require(amount > 0, "Token quantity must be positive");
+        // require that the user has enough tokens
+        require(balanceByDay[day][seller] >= amount, "Your balance is insufficient");
+        // get the price of the requested quantity of tokens
+        salePrice = getSalePrice(amount);
+
+        // burn the sold tokens
+        _burn(seller, amount);
+        // update _totalSupply
+        _totalSupply = totalSupply();
+        // and update seller's balance
+        balanceByDay[day][seller] = balanceOf(seller);
+
+        // pay the seller
+        (bool callSuccess, ) = payable(seller).call{value: salePrice}("");
+        require(callSuccess, "Payment failed");
+
+        // update the reserve balance of this contract
+        reserveBalance -= salePrice;
+
+        // finally reset saleePrice to 0 so that the next time the user calls
+        // salePrice getter function they do not see the value of the last
+        // sale
+        salePrice = 0;
+    }
+
+    receive () external payable {
+        // setting amount = 0 will cause buyTokens() function to revert
+        uint256 amount = 0;
+        buyTokens(amount);
+    }
+
+    fallback() external payable {
+        // setting amount = 0 will cause buyTokens() function to revert
+        uint256 amount = 0;
+        buyTokens(amount);
     }
 }
