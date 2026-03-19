b78a34b361e4dc662c125b48d8cf1563dca3e6b0
auppal1 <abhayuppal@hotmail.co.uk>
2026-03-19 17:48:14 +0000

    server.js - initialise express server env.js - chore: add env config web3.js - linked sepolia provder index.js - created API routes

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use('/api', require('./routes/index'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API Gateway running on port ${PORT}`));
+require('./config/web3');
