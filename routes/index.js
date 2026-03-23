c2ac1e7aaa82d4f9c0b93152002fafb01038407b
abhay <c1050555@newcastle.ac.uk>
2026-03-23 13:32:22 +0000

    contracts.js: Added contract instance configuration using ethers.js and Smart contract ABI

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const verifyToken = require('../middleware/verifyToken');
+
+router.get('/protected', verifyToken, (req, res) => {
+    res.json({ message: 'Access granted - Token verified' });
+});
 
 router.get('/health', (req, res) => res.json({ status: 'ok' }));
 router.post('/request', (req, res) => res.json({ message: 'Token check TBD' }));
 router.get('/balance/:walletAddress', (req, res) =>
   res.json({ wallet: req.params.walletAddress, balance: 'stub TBD' })
 );
+
+
 
 module.exports = router;
