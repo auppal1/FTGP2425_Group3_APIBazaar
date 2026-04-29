eb720ba0eb0bbb7cc7891eda4ba9761f2409fc88
abhay <c1050555@newcastle.ac.uk>
2026-04-29 15:12:37 +0100

    Enhance verifyToken middleware to include listing address validation and adjust balance check logic

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,22 +1,27 @@
 const{contract} = require('../config/contracts');
 
+
 const verifyToken = async (req, res, next) => {
-    const {walletAddress} = req.body;
+    const {walletAddress, listingAddress} = req.body;
+
 
     if (!walletAddress) {
         return res.status(400).json({ error: 'Wallet address is required' });
     }
+    if (!listingAddress) {
+        return res.status(400).json({ error: 'Listing address is required' });
+    }
     try{
-        const balance = await contract.getCurrentDayBalance(walletAddress);
+        const balance = await contract.getCurrentDayBalance(listingAddress, walletAddress);
         if (balance <= 0n) {
-            return res.status(403).json({ error: 'Insufficient token balance' }); // wallet has tokens
-        } else {
-            return res.status(403).json({ error: 'Access denied: No tokens found' });
+            return res.status(403).json({ error: 'Access denied: No tokens found'  }); // wallet has tokens
         }
+        next();
     }
     catch(error){
+        console.error({'verifyToken error': error.message});
         res.status(500).json({ error: 'Error verifying token' });
     }
 };
 
-    module.exports = verifyToken;
+module.exports = verifyToken;
